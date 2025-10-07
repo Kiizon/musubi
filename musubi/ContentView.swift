c159b88bef9806beb19ce5da5a3dbade5ad5a63c
Kiizon <kishddizon@gmail.com>
2025-10-07 19:50:24 -0400

    Initial Commit

---
+++ musubi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  musubi
+//
+//  Created by Kish Dizon on 2025-10-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ musubi/musubiApp.swift
@@ -0,0 +1,17 @@
+//
+//  musubiApp.swift
+//  musubi
+//
+//  Created by Kish Dizon on 2025-10-07.
+//
+
+import SwiftUI
+
+@main
+struct musubiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
