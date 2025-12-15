55bfcb92abb8a9349dbd1799696596065cdf1d3a
Kiizon <kishddizon@gmail.com>
2025-12-15 00:35:50 -0500

    Update cask SHA256 for v1.0.0

--- Casks/musubi.rb
+++ Casks/musubi.rb
@@ -1,6 +1,6 @@
 cask "musubi" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_SHA256_AFTER_RELEASE"
+  sha256 "9931dcbad2768e3633050f6d2be57bf4bde5d0effedabf75e333de6b8fb0f07f"
 
   url "https://github.com/Kiizon/musubi/releases/download/v#{version}/musubi-#{version}.zip"
   name "musubi"
